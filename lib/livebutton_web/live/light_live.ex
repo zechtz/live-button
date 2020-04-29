3068bf10a3a339269cf61739aa705ef7bed8191b
Zachariah Ngonyani <zachariahn@gmail.com>
2020-04-30 02:34:40 +0300

    Finish Liveview button

--- lib/livebutton_web/live/light_live.ex
+++ lib/livebutton_web/live/light_live.ex
@@ -8,16 +8,26 @@ defmodule LivebuttonWeb.LightLive do
 
   def render(assigns) do
     ~L"""
-    <h1>Front Porch Light</h1>
+    <h1>Light Button</h1>
     <div id="light">
       <div class="meter">
         <span style="width: <%= @brightness %>%">
         <%= @brightness %>%
         </span>
       </div>
+
       <button phx-click="off">
         <img src="/images/light-off.svg" />
       </button>
+
+      <button phx-click="down">
+        <img src="/images/down.svg" />
+      </button>
+
+      <button phx-click="up">
+        <img src="/images/up.svg" />
+      </button>
+
       <button phx-click="on">
         <img src="/images/light-on.svg" />
       </button>
@@ -27,6 +37,17 @@ defmodule LivebuttonWeb.LightLive do
 
   def handle_event("on", _, socket) do
     socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    brightness = socket.assigns.brightness + 10
+    socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
     {:noreply, socket}
   end
 
