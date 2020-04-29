ba8c70f615546b71ce459a395aa35407e440df8d
Zachariah Ngonyani <zachariahn@gmail.com>
2020-04-30 02:07:51 +0300

    update config

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :livebutton, Livebutton.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "secret",
   database: "livebutton_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
