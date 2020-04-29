8628798ffec997c1e0c596f83f5e9479c5afc7db
Zachariah Ngonyani <zachariahn@gmail.com>
2020-04-30 02:20:18 +0300

    Add on and off events

--- lib/livebutton_web/live/light_live.ex
+++ lib/livebutton_web/live/light_live.ex
@@ -11,11 +11,26 @@ defmodule LivebuttonWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style="width: <%= @brightness %>px">
-        <%= @brightness %>
+        <span style="width: <%= @brightness %>%">
+        <%= @brightness %>%
         </span>
       </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" />
+      </button>
     </div>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    {:noreply, assign(socket, :brightness, 0)}
   end
 end
